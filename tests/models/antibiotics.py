c5fdda1c2ea98e38fef1803b20d623bc0f138d32
Kitsu272 <t.marin@utp.edu.co>
2024-10-21 09:08:38 -0500

    cleaning Update

--- tests/models/antibiotics.py
+++
@@ -1,9 +0,0 @@
-class antibiotics:
-    def __init__(self, productName , dose, price , animalsForDose ):
-        self.productName = productName
-        self.dose = dose
-        self.price = price
-        self.animalsForDose = animalsForDose
-       
-
-    
--- tests/models/client.py
+++
@@ -1,8 +0,0 @@
-class client:
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
-        self.history = [] 
-
-    def purchaseHistory(self):
-        return self.history 
